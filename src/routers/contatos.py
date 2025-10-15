015de788ea72778e55f9ae84ecd40cb1a9d65a61
matheus lobo <klebercamara12@gmail.com>
2025-10-15 08:55:20 -0400

    feat: Implementa endpoint PUT para atualizar Contato por ID
    - Adiciona o endpoint PUT /contatos/{id} para substituição completa de um contato.
    - Retorna um erro 404 se o contato não for encontrado.
    
    Co-authored-by: paoeflor <maruzacloud@gmail.com>

--- src/routers/contatos.py
+++ src/routers/contatos.py
@@ -3,7 +3,10 @@ from src.models.contato import Contato
 from src.db.memoria_db import CONTATOS, PROXIMO_ID
 from typing import List
 
-router = APIRouter(prefix="/contatos", tags=["Contatos"])
+router = APIRouter(
+    prefix="/contatos",
+    tags=["Contatos"]
+)
 
 
 @router.post("/", response_model=Contato, status_code=201)
@@ -28,4 +31,18 @@ async def buscar_contato_por_id(contato_id: int):
         if contato.id == contato_id:
             return contato
 
+    raise HTTPException(status_code=404, detail="Contato não encontrado")
+
+
+@router.put("/{contato_id}", response_model=Contato)
+async def atualizar_contato(contato_id: int, contato_atualizado: Contato):
+    for index, contato in enumerate(CONTATOS):
+        if contato.id == contato_id:
+            
+            novo_contato = contato_atualizado.model_copy(update={"id": contato_id})
+            
+            CONTATOS[index] = novo_contato
+            
+            return novo_contato
+            
     raise HTTPException(status_code=404, detail="Contato não encontrado")

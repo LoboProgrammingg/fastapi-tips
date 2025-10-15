b747446eb6a92a4098b219b15367578f5a9ddcdd
matheus lobo <klebercamara12@gmail.com>
2025-10-15 08:51:19 -0400

    feat: Adiciona endpoint GET para listar todos os Contatos
    - Implementa o endpoint GET /contatos para listar o banco de dados em memória.
    - Melhora a tipagem para List[Contato].
    
    Co-authored-by: paoeflor <maruzacloud@gmail.com>

--- src/routers/contatos.py
+++ src/routers/contatos.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from src.models.contato import Contato
 from src.db.memoria_db import CONTATOS, PROXIMO_ID
+from typing import List
 
 router = APIRouter(
     prefix="/contatos",
@@ -9,11 +10,14 @@ router = APIRouter(
 
 @router.post("/", response_model=Contato, status_code=201)
 async def criar_contato(contato: Contato):
-
     global PROXIMO_ID
     
     contato.id = PROXIMO_ID
-    CONTATOS.append(contato)
+    CONTATOS.append(contato.model_copy(update={"id": PROXIMO_ID})) 
     PROXIMO_ID += 1
 
     return contato
+
+@router.get("/", response_model=List[Contato])
+async def listar_contatos():
+    return CONTATOS
